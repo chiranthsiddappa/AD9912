f8518809908fec28c793c926458c89ef2244adea
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2014-05-08 02:23:02 -0600

    Data added to header file

---
+++ AmpDataParser.rb
@@ -0,0 +1,21 @@
+min = "dBMMinVals = {"
+max = "dBMMaxVals = {"
+
+ampDataHashArr = []
+File.open("AmplitudeCalibrationData.csv").each do |line|
+  lineSplit = line.split(",")
+  ampDataHashArr.push({"frequency" => lineSplit[0].to_i, "min" => lineSplit[1].to_f, "max" => lineSplit[2].to_f})
+end
+
+ampDataHashArr.each do |dat|
+  if(dat["frequency"] >= 60)
+    min += "#{dat["min"]},"
+    max += "#{dat["max"]},"
+  end
+end
+
+min += "};"
+max += "};"
+
+puts min
+puts max
