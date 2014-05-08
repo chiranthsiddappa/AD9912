2a9abdcdc3b3f68c65cd0545b2dad831c9da47d2
Chiranth Siddappa <chiranthsiddappa@gmail.com>
2014-05-08 16:14:39 -0600

    Chip funcitonal with demo, menu, and changed data points

--- AD9912_sketch/AD9912_sketch.ino
+++ AD9912_sketch/AD9912_sketch.ino
@@ -41,20 +41,34 @@ uint32_t userFreq = 1;
 uint16_t userDAC = 0;
 uint32_t freqCursPos = 0;
 uint32_t DACCursPos = 0;
+uint8_t menuCursPos = 0;
 uint8_t green_mode = 0;
 uint8_t menu_mode = 0;
+uint32_t resetFreq;
+uint16_t resetDAC;
+uint16_t demoState = 0x0;
 
 #define green_freq_mode 0x0
 #define green_dac_mode 0x1
 #define menu_init 0x0
 #define menu_ff 0x1
+#define menu_demo 0x2
+#define demo_freq_1_uc 0x1
+#define demo_freq_2_uc 0x2
+#define demo_freq_3_uc 0x3
+#define demo_freq_4_uc 0x4
+#define demo_freq_1_c 0x5
+#define demo_freq_2_c 0x6
+#define demo_freq_3_c 0x7
+#define demo_freq_4_c 0x8
+#define demo_DBM_match -4.904418
 
 AD9912 ad9912;
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 void setup()
 {
-  Serial.begin(9600);
+  delay(200);  
   //UI pins
   pinMode(GREEN_BUTTON, INPUT);
   pinMode(RED_BUTTON, INPUT);
@@ -73,7 +87,6 @@ void setup()
   // put your setup code here, to run once:
   pinMode(BLUE_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
-  Serial.begin(9600);
   //Initialize other DDS pins
   pinMode(IO_update, OUTPUT);
   pinMode(SPISCK, OUTPUT);
@@ -94,6 +107,9 @@ void setup()
     flash_green();
     userFreq = ad9912.getFrequency();
     userDAC = ad9912.DAC_read();
+    resetFreq = userFreq;
+    resetDAC = userDAC;
+    displayMenu();
   }
   else {
     lcd.print("Chip Not Found");
@@ -104,15 +120,93 @@ void setup()
 void loop()
 {
   if(menu_mode == menu_init) {
+    displayMenuCursor();
     if(digitalRead(GREEN_BUTTON) == HIGH) {
-      menu_mode = menu_ff;
-      for(int i = 0; i < 4; i++) {
-	lcd.clearRow(i);
+      if(menuCursPos == 0x0) {
+	menu_mode = menu_ff;
+	for(int i = 0; i < 4; i++) {
+	  lcd.clearRow(i);
+	}
+	printUserFreq();
+	printUserDAC();
+	printUserDBM();
+	printUserVpp();
       }
+      else if (menuCursPos == 0x1) {
+	menu_mode = menu_demo;
+	resetFreq = userFreq;
+	resetDAC = userDAC;
+      }
+    }
+    else if(digitalRead(UP_BUTTON) == HIGH) {
+      incMenuCursor();
+      delay(200);
+    }
+    else if(digitalRead(DOWN_BUTTON) == HIGH) {
+      decMenuCursor();
+      delay(200);
+    }
+  }
+  else if (menu_mode == menu_demo) {
+    // demo stuff here
+    if(digitalRead(RED_BUTTON) == HIGH) {
+      menu_mode = menu_init;
+      displayMenu();
+      userFreq = resetFreq;
+      userDAC = resetDAC;
+      ad9912.setFrequency(userFreq);
+      ad9912.DAC_write(userDAC);
+      delay(200);
+    }
+    else if (digitalRead(GREEN_BUTTON) == HIGH) {
+      if(demoState < 0x8) {
+	++demoState;
+      } else 
+	demoState = demo_freq_1_uc;
+      if(demoState == demo_freq_1_uc) {
+	userFreq = 195 * pow(10 , 6);
+	userDAC = 0x1FF;
+      } else if (demoState == demo_freq_2_uc) {
+	userFreq = 362 * pow(10 , 6);
+	userDAC = 0x1FF;
+      } else if (demoState == demo_freq_3_uc) {
+	userFreq = 385 * pow(10 , 6);
+	userDAC = 0x1FF;
+      }
+      else if (demoState == demo_freq_4_uc) {
+	userFreq = 400 * pow(10 , 6);
+	userDAC = 0x1FF;
+      }
+      else if (demoState == demo_freq_1_c) {
+	userFreq = 195 * pow(10 , 6);
+	//	userDAC = findBestDACMatch(userFreq , demo_DBM_match);
+	userDAC = 180;
+      }
+      else if (demoState == demo_freq_2_c) {
+	userFreq = 362 * pow(10 , 6);
+	//	userDAC = findBestDACMatch(userFreq , demo_DBM_match);
+	userDAC = 820;
+      }
+      else if (demoState == demo_freq_3_c) {
+	userFreq = 385 * pow(10 , 6);
+	//	userDAC = findBestDACMatch(userFreq , demo_DBM_match);
+	userDAC = 590;
+      }
+      else if (demoState == demo_freq_4_c) {
+	userFreq = 400 * pow(10 , 6);
+	//	userDAC = findBestDACMatch(userFreq , demo_DBM_match);
+	userDAC = 511;
+      }
+      // Constant display stuff here
+      ad9912.setFrequency(userFreq);
+      ad9912.DAC_write(userDAC);
       printUserFreq();
       printUserDAC();
+      printUserDBM();
+      printUserVpp();
+      delay(150);
     }
-  } 
+  }
   else if (menu_mode == menu_ff) {
     //display cursor
     if(green_mode == green_freq_mode)
@@ -132,7 +226,7 @@ void loop()
 	ad9912.setFrequency(userFreq);
 	printUserDBM();
 	printUserVpp();
-	delay(200);
+	delay(150);
       }
       if(digitalRead(DOWN_BUTTON) == HIGH) {
 	decUserFreq();
@@ -140,7 +234,7 @@ void loop()
 	ad9912.setFrequency(userFreq);
 	printUserDBM();
 	printUserVpp();
-	delay(200);
+	delay(150);
       }
       if(digitalRead(LEFT_BUTTON) == HIGH) {
 	incFreqCurs();
@@ -158,7 +252,7 @@ void loop()
 	ad9912.DAC_write(userDAC);
 	printUserDBM();
 	printUserVpp();
-	delay(200);
+	delay(150);
       }
       if(digitalRead(DOWN_BUTTON) == HIGH) {
 	decUserDAC();
@@ -166,7 +260,7 @@ void loop()
 	ad9912.DAC_write(userDAC);
 	printUserDBM();
 	printUserVpp();
-	delay(200);
+	delay(150);
       }
       if(digitalRead(LEFT_BUTTON) == HIGH) {
 	incDACCurs();
@@ -176,6 +270,10 @@ void loop()
 	decDACCurs();
 	delay(200);
       }
+    }
+    if(digitalRead(RED_BUTTON) == HIGH) {
+      menu_mode = menu_init;
+      displayMenu();
     }
   }
 }
@@ -208,13 +306,6 @@ uint ad9912_frequency_sweep() {
   }
   ad9912.FTW_write(orig_FTW);
   return 1;
-}
-
-void LCD_clear_row(short row) {
-  for(int i = 1; i <= 20; i++) {
-    lcd.setCursor(row, i);
-    lcd.print(" ");
-  }
 }
 
 void LCDDisplayCurrentSettings() {
@@ -282,7 +373,7 @@ void printUserDAC() {
 void printUserDBM() {
   lcd.noCursor();
   lcd.clearRow(2);
-  if(userFreq >= ACD_start && userFreq <= ACD_stop) {
+  if(userFreq >= ACD_start * pow(10 , 6) && userFreq <= ACD_stop * pow(10 , 6)) {
     lcd.setCursor(0,2);
     lcd.print("dBM: ");
     lcd.print(getDBM(userFreq , userDAC) , 4);
@@ -293,7 +384,7 @@ void printUserDBM() {
 void printUserVpp() {
   lcd.noCursor();
   lcd.clearRow(3);
-  if(userFreq >= ACD_start && userFreq <= ACD_stop) {
+  if(userFreq >= ACD_start * pow(10 , 6) && userFreq <= ACD_stop * pow(10 , 6)) {
     lcd.setCursor(0,3);
     lcd.print("Vpp: ");
     lcd.print(getVpp(userFreq , userDAC) , 4);
@@ -361,4 +452,37 @@ void decUserDAC() {
   uint32_t decAmount = (uint32_t) pow(10, DACCursPos);
   if(decAmount <= userDAC)
     userDAC -= decAmount;
+}
+
+void displayMenu() {
+  lcd.noCursor();
+  lcd.noBlink();
+  for(int i = 0; i <= 3; i++) {
+    lcd.clearRow(i);
+  }
+  lcd.setCursor(0,0);
+  lcd.print(" Full Function");
+  lcd.setCursor(0,1);
+  lcd.print(" Demo");
+  displayMenuCursor();
+}
+
+void displayMenuCursor() {
+  lcd.cursor();
+  lcd.setCursor(0 , menuCursPos);
+}
+
+void incMenuCursor() {
+  if(menuCursPos < 0x1) {
+    ++menuCursPos;
+  }
+  else 
+    menuCursPos = 0x0;
+}
+
+void decMenuCursor() {
+  if(menuCursPos > 0x0) 
+    --menuCursPos;
+  else 
+    menuCursPos = 0x1;
 }
